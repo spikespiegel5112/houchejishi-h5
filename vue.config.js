4e6f1f519470065824109eb21c145e096ac609e1
spikespiegel5112 <583288277@qq.com>
2020-05-29 15:09:32 +0800

    temp

--- vue.config.js
+++ vue.config.js
@@ -115,10 +115,10 @@ module.exports = {
                   priority: 10,
                   chunks: 'initial' // only package third parties that are initially dependent
                 },
-                elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
+                vant: {
+                  name: 'chunk-vant', // split vant into a single package
                   priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                  test: /[\\/]node_modules[\\/]_?vant(.*)/ // in order to adapt to cnpm
                 },
                 commons: {
                   name: 'chunk-commons',
